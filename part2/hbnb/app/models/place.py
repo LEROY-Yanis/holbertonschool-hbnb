45203329bd92913b79b827686c5c2f18851e6d03
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-27 16:47:28 +0100

    Add attributes to Place model: title, price, latitude, longitude, and owner with validation

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,10 +1,26 @@
 import uuid
 
 class Place:
-	def __init__(self, name, description=None, id=None):
+	def __init__(self, name, description=None, title=None, id=None, price=None, latitude=None, longitude=None, owner=None):
+		if title is None:
+			raise ValueError("title is empty")
+		if price <= 0:
+			raise ValueError("the price must be bigger of 0") 
+		if len(latitude) < 90 or len(latitude) > 90:
+			raise ValueError("latitude must be between -90 and 90")
+		if len(longitude) < 180 or len(longitude) > 180:
+			raise ValueError("longitude must be between -180 and 180")
+
 		self.id = id or str(uuid.uuid4())
 		self.name = name
 		self.description = description
+		self.title = title
+		self.price = price
+		self.latitude = latitude
+		self.longitude = longitude
+		self.owner = owner
+		self.reviews = []  # List to store related reviews
+		self.amenities = []  # List to store related amenities
 
 	def to_dict(self):
 		return {'id': self.id, 'name': self.name, 'description': self.description}
