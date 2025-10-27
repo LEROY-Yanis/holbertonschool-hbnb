a60f66578745836f37472ac9d0d0dc8ee7cbf3e5
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-27 17:56:58 +0100

    Add validation for Review model attributes: place_id, user_id, and text

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -2,6 +2,13 @@ import uuid
 
 class Review:
 	def __init__(self, place_id, user_id, text, id=None):
+		if text is None:
+			raise ValueError("text is empty")
+		if not place_id:
+			raise ValueError("place_id is required")
+		if not user_id:
+			raise ValueError("user_id is required")
+		
 		self.id = id or str(uuid.uuid4())
 		self.place_id = place_id
 		self.user_id = user_id
