fc726c70b2c9dee973811e3a9cf35ef02ed7793c
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-28 15:07:57 +0100

    Add place model definition with fields for API integration

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -22,6 +22,18 @@ class Place:
 		self.reviews = []  # List to store related reviews
 		self.amenities = []  # List to store related amenities
 
+		place_model = api.model('Place', {
+    		'title': fields.String(required=True, description='Title of the place'),
+    		'description': fields.String(description='Description of the place'),
+    		'price': fields.Float(required=True, description='Price per night'),
+    		'latitude': fields.Float(required=True, description='Latitude of the place'),
+    		'longitude': fields.Float(required=True, description='Longitude of the place'),
+    		'owner_id': fields.String(required=True, description='ID of the owner'),
+    		'owner': fields.Nested(user_model, description='Owner of the place'),
+    		'amenities': fields.List(fields.Nested(amenity_model), description='List of amenities'),
+    		'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
+		})
+
 	def to_dict(self):
 		return {'id': self.id, 'name': self.name, 'description': self.description}
 
