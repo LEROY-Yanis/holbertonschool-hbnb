13713f569f3e7e71cebb2da4b93ac93d0e6bf6ba
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-28 15:07:57 +0100

    Add review model definition with fields for API integration

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -14,6 +14,14 @@ class Review:
 		self.user_id = user_id
 		self.text = text
 
+		# Adding the review model
+		review_model = api.model('PlaceReview', {
+    		'id': fields.String(description='Review ID'),
+    		'text': fields.String(description='Text of the review'),
+    		'rating': fields.Integer(description='Rating of the place (1-5)'),
+    		'user_id': fields.String(description='ID of the user')
+		})
+
 	def to_dict(self):
 		return {'id': self.id, 'place_id': self.place_id, 'user_id': self.user_id, 'text': self.text}
 
