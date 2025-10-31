9f77acfc8307fbe9d67abfcb2f14dd1ac5d9e8c1
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-31 10:51:07 +0100

    Add API namespace imports for users, amenities, places, and reviews

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -9,6 +9,10 @@ def create_app():
     api = Api(app, version='2.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/docs')
 
     # Placeholder for API namespaces (endpoints will be added later)
+    from .api.v1.users import api_usr
+    from .api.v1.amenities import api_amnt
+    from .api.v1.places import api_plc
+    from .api.v1.reviews import api_rvw
     # Additional namespaces for places, reviews, and amenities will be added later
 
     return app
