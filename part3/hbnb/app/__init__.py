c417d6c60fff6963697798f271d90fd96bed21d3
LEROY-Yanis <11479@holbertonstudents.com>
2025-11-09 21:38:42 +0100

    Add configuration class parameter to create_app function

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,8 +4,9 @@ from app.services.facade import HBnBFacade
 
 facade = HBnBFacade()
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='2.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/docs')
 
     # Placeholder for API namespaces (endpoints will be added later)
