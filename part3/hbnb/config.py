be6a3a97d4de51e84501c04aa2e3b60b20d5eab5
LEROY-Yanis <11479@holbertonstudents.com>
2025-11-12 16:31:03 +0100

    Add JWT configuration to the Config class

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -1,8 +1,11 @@
 import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 
 class DevelopmentConfig(Config):
     DEBUG = True
